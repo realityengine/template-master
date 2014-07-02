e689409077c2bd496dec24a61a1f88def49f76c0
Frank Diaz <frank.diaz@realityengine.net>
2014-07-01 20:07:21 -0400

    Initial Submission

---
+++ template/template/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  template
+//
+//  Created by realityengine on 6/29/14.
+//  Copyright (c) 2014 Frank Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ template/templateTests/templateTests.m
@@ -0,0 +1,34 @@
+//
+//  templateTests.m
+//  templateTests
+//
+//  Created by realityengine on 6/29/14.
+//  Copyright (c) 2014 Frank Diaz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface templateTests : XCTestCase
+
+@end
+
+@implementation templateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

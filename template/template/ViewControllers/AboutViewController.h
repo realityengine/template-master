8f3c12668ebe4843b46d623caa9f8413caacafc7
Frank Diaz <frank.diaz@realityengine.net>
2014-07-01 21:54:43 -0400

    AboutViewController files.

---
+++ template/template/ViewControllers/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  template
+//
+//  Created by realityengine on 7/1/14.
+//  Copyright (c) 2014 realityengine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ template/template/ViewControllers/AboutViewController.m
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.m
+//  template
+//
+//  Created by realityengine on 7/1/14.
+//  Copyright (c) 2014 realityengine. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+@end
